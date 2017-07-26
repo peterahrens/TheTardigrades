b14e0d0fd9a2ff388957ed88b4b162669a304027
Peter Ahrens <ptrahrens@gmail.com>
2017-07-26 11:32:24 -0400

    made some great image function

--- image.c
+++ image.c
@@ -1,50 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <limits.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 void dump_gs(const char* name, const size_t width, const size_t height, const uint8_t *data){
-  char fname[PATH_MAX];
+  char fname[strlen(name) + 5];
   strcpy(fname, name);
   strcat(fname, ".pgs");
-  FILE file = open(fname, "w");
+  FILE *file = fopen(fname, "w");
   fprintf(file, "P5\n");
   fprintf(file, "%zd, %zd\n", width, height);
   fprintf(file, "255\n");
-  for(size_t i = 0; i < width * height; i++){
-    uint8_t mask = 1;
-    while(mask < 256){
-      if (mask & data[i]){
-        fprintf(file, "1");
-      } else {
-        fprintf(file, "0");
-      }
-    }
-  }
-  fprintf(file, "\n");
-  close(fname);
+  fwrite(data, sizeof(uint8_t), width * height, file);
+  fclose(file);
 }
 
 void dump_rgb(const char* name, const size_t width, const size_t height, const uint8_t *data){
-  char fname[PATH_MAX];
+  char fname[strlen(name) + 5];
   strcpy(fname, name);
   strcat(fname, ".ppm");
-  FILE file = open(fname, "w");
+  FILE *file = fopen(fname, "w");
   fprintf(file, "P6\n");
   fprintf(file, "%zd, %zd\n", width, height);
   fprintf(file, "255\n");
-  for(size_t i = 0; i < width * height * 3; i++){
-    uint8_t mask = 1;
-    while(mask < 256){
-      if (mask & data[i]){
-        fprintf(file, "1");
-      } else {
-        fprintf(file, "0");
-      }
-    }
-  }
-  fprintf(file, "\n");
-  close(fname);
+  fwrite(data, sizeof(uint8_t), width * height, file);
+  fclose(file);
 }
 
 
@@ -52,10 +33,10 @@ int main(int argc, char** argv){
   int radius = 16;
   int width = radius;
   int height = radius;
-  data[width * height];
+  uint8_t data[width * height];
   for(int i = 0; i < width; i++){
     for(int j = 0; j < height; j++){
-      data[j * width + i] = (width * width + height * height < radius * radius) * 255;
+      data[j * width + i] = (i * i + j * j < radius * radius) * 255;
     }
   }
   dump_gs("mycoofile", width, height, data);

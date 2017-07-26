6ce2e4d553bf938bd96068975cbd185cc20534d8
Hannah Klion <klionhe@ornl.gov>
2017-07-26 13:30:24 -0400

    should work

--- mandelbrot.c
+++ mandelbrot.c
@@ -2,35 +2,54 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-uint8_t* mandelbrot(float x_init, float y_init)
+void mandelbrot(double centerx, double centery, double lengthx, double lengthy, int pixel_countx, 
+    double* hsv)
 {
   int iter = 0;
   int iter_max = 1000;
-  float radius = 0;
-  float x, y;
-  float radius_max = 2.0;
-  uint8_t rgb[3];
-  
-  x = x_init;
-  y = y_init;
-  while (radius < radius_max && iter < iter_max)
-  {
-    x = x * x - y * y + x_init;
-    y = 2.0 * x * y + y_init;
-    iter++;
-  }
-  if (iter < iter_max)
-  {
-    *rgb = 255;
-    *(rgb + 1) = 255;
-    *(rgb + 2) = 255;
-  }
-  else
-  {
-    *rgb = 0;
-    *(rgb + 1) = 0;
-    *(rgb + 2) = 0;
-  }
+  int pixel_county;
+  double radius = 0;
+  double x, y, minx, maxy, pixel_size, init_x, init_y
+  double radius_max = 2.0;
 
+  minx = centerx - lengthx / 2.0;
+  maxy = centery + lengthy / 2.0;
+  pixel_size = lengthx / (double)pixel_countx;
+  pixel_county = (int)(lengthy / pixel_size);
+
+  for (pixely = 0; pixely < pixel_county; pixely++)
+  {
+    for (pixelx = 0; pixelx < pixel_countx; pixelx++)
+    {
+      x_init = minx + pixelx * pixel_size;
+      y_init = maxy - pixely * pixel_size;
+      i = (pixel_county * pixely + pixelx) * 3;
+      
+      x = x_init;
+      y = y_init;
+      while (radius < radius_max && iter < iter_max)
+      {
+        x = x * x - y * y + x_init;
+        y = 2.0 * x * y + y_init;
+        iter++;
+      }
+      if (iter < iter_max)
+      {
+        printf("true %f %f\n", (x_init, y_init));
+        hsv[i] = 0.;
+        hsv[i+1] = 1.;
+        hsv[i+2] = 0.;
+      }
+      else
+      {
+        printf("false %f %f\n", (x_init,y_init));
+        hsv[i] = 0.;
+        hsv[i+1] = 0;
+        hsv[i+2] = 0;
+      }
+
+    }
+  }
+  
   return rgb;
 }
